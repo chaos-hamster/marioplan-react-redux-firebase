c0466f4975081a765da3b764850919efc70ecaea
Mark Sweeney <mark@ninereeds.co.uk>
2021-05-29 22:38:08 +0100

    updated read.me

---
+++ src/components/layout/SignedOutLinks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+export default function SignedOutLinks() {
+    return (
+        <div>
+            <ul className="right">
+                <li><NavLink to='/signin'>Login</NavLink></li>
+                <li><NavLink to='/signup'>Signup</NavLink></li>
+            </ul>
+        </div>
+    )
+}
